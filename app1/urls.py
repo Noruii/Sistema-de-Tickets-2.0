9b64b31e6c96422c701485f7865b4573aa709866
Norbert <norbertfenix@hotmail.com>
2023-12-10 03:48:50 -0400

    implementando searchPanes

--- app1/urls.py
+++ app1/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from app1.views.usuarios.views import usuarios_list
 from django.contrib.auth import views as auth_views
+from app1.views.usuarios.views import *
 
 urlpatterns = [
     # path('uno/', views.testview, name='view1'),
     # path('dos/', views.testview2, name='view2'),
-    path('usuarios/list/', usuarios_list, )
+
+    path('usuarios/list/', usuarios_list, name='usuarios_list')
+    #path('usuarios/list/', UsuariosListView.as_view(), name='usuarios_list')
 ]
--- app1/views/usuarios/views.py
+++ app1/views/usuarios/views.py
@@ -1,9 +1,13 @@
+from typing import Any
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView
 
 from app1.models import *
 
 # Create your views here.
+
+# Vistas basada en funcion
 def usuarios_list(request):
     data = {
         'title': 'Listado de usuarios',
@@ -11,14 +15,16 @@ def usuarios_list(request):
     }
     return render(request, 'usuarios/list.html', data)
 
-# def testview(request):
-#     return render(request, 'index.html', {
-#         'name':'pepe',
-#         'tickets': Ticket.objects.all()
-#     })
+# buena explicacion https://youtu.be/iP4LxWbbpy8?si=A5hF7fx466GCTfD3
+# ListView | Vistas basadas en clases (manda 'object_list' a la plantilla html revisar doc django)
+class UsuariosListView(ListView):
+    model = User
+    template_name = "usuarios/list.html"
 
-# def testview2(request):
-#     return render(request, 'test.html', {
-#         'name':'pepe',
-#         'usuarios': User.objects.all()
-#     })
+    # sobrescreebir este metodo que devuelve el diccionario que representa el contexto de la platilla
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Listado de usuarios'
+        # context['object_list'] = Ticket.objects.allK() # puedes sobreescribir el atributo 'object_list' para que en vez del modelo 'User' mande otro modelo
+        return context
+    
